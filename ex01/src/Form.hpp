9081920f6a3d5127fc278e3c4699a8e88f9a4308
lschrafs <lschrafs@student.42wolfsburg.de>
2022-10-07 10:07:31 +0200

    ex01 finished

---
+++ ex01/src/Form.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 11:25:14 by lschrafs          #+#    #+#             */
+/*   Updated: 2022/10/06 11:52:35 by lschrafs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_H
+#define FORM_H
+
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form {
+ public:
+	// Constructors
+	Form(std::string name, int grade_for_signing, int grade_for_execution);
+	Form(const Form &copy);
+
+	// Destructor
+	~Form();
+
+	// Getters & Setters
+	bool getSignedStatus() const;
+	std::string getName() const;
+	int getGradeSign() const;
+	int getGradeExecute() const;
+
+	// Functions
+	void beSigned(const Bureaucrat &bureaucrat);
+
+	// Exceptions
+	class GradeTooHighException : public std::exception {
+	 public:
+		virtual const char *what() const throw();
+	};
+	
+  class GradeTooLowException: public std::exception {
+	 public:
+		virtual const char *what() const throw();
+	};
+
+ private:
+	Form();
+	Form &operator=(const Form &);
+	
+	const std::string name_;
+	bool signed_;
+	const int grade_sign_;
+	const int grade_execute_;
+};
+
+std::ostream& operator<<(std::ostream& out, const Form& form);
+
+#endif
---
+++ ex01/src/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 09:04:49 by lschrafs          #+#    #+#             */
+/*   Updated: 2022/10/07 10:06:42 by lschrafs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+
+int main(void) {
+	std::cout << RED << "----- invalid form creation -----" << std::endl << std::endl;
+	try {
+		Form f1("Something", 1, 0);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		Form f2("Something", 1, 151);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl << GREEN << "----- trying to sign some forms -----" << std::endl << std::endl;
+	
+	Bureaucrat abby("Abby", 75);
+	Form f3("Average form", 75, 150);
+	Form f4("Presidential form", 74, 150);
+	Form f5(f3);
+	Form f6(f4);
+	abby.signForm(f3);
+	abby.signForm(f4);
+	abby.signForm(f5);
+	abby.signForm(f6);
+}
